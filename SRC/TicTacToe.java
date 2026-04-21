8ed228090b1043d1bdd2ed3247dd93aa6872ddc8
Siva Sagar <sk2076@srmist.edu.in>
2026-04-21 15:27:44 +0530

    UC2 done

--- SRC/TicTacToe.java
+++ SRC/TicTacToe.java
@@ -1,31 +1,41 @@
+import java.util.Random;
+
 public class TicTacToe {
 
-    static char[][] board = new char[3][3];
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
 
     public static void main(String[] args) {
-        initializeBoard();
-        printBoard();
+        tossAndAssignSymbols();
+        displayTossResult();
     }
 
-    /** Fills each cell with '-' to mark it empty. */
-    static void initializeBoard() {
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                board[row][col] = '-';
-            }
+    static void tossAndAssignSymbols() {
+        Random random = new Random();
+        int toss = random.nextInt(2); // 0 or 1
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
         }
     }
 
-    /** Prints the board with separators. */
-    static void printBoard() {
-        System.out.println("-----------");
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                System.out.print(board[row][col]);
-                if (col < 2) System.out.print(" | ");
-            }
-            System.out.println();
-            System.out.println("-----------");
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("You won the toss!");
+            System.out.println("You play first.");
+        } else {
+            System.out.println("Computer won the toss!");
+            System.out.println("Computer plays first.");
         }
+
+        System.out.println("Your symbol: " + humanSymbol);
+        System.out.println("Computer's symbol: " + computerSymbol);
     }
 }
