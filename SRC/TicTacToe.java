642bea02ef813b2b2db64781c1d9da5a7028b6ee
Siva Sagar <sk2076@srmist.edu.in>
2026-04-21 15:49:10 +0530

    UC4 done

--- SRC/TicTacToe.java
+++ SRC/TicTacToe.java
@@ -3,20 +3,19 @@ import java.util.Scanner;
 public class TicTacToe {
 
     public static void main(String[] args) {
-        int slot = getUserSlot();
-        System.out.println("Slot entered: " + slot);
-    }
-
-    /**
-     * Reads an integer slot value from the user.
-     * Input: Scanner object
-     * Output: Slot number (1-9)
-     * Note: Validation will be added in later use cases.
-     */
-    static int getUserSlot() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a slot number (1-9): ");
         int slot = scanner.nextInt();
-        return slot;
+
+        System.out.println("Row: " + getRowFromSlot(slot));
+        System.out.println("Column: " + getColFromSlot(slot));
+    }
+
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
+
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
     }
 }
